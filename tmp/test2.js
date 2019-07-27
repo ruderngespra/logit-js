eb664afbcac9fbb5874edc28f8059dba73a5af82
janko <janko_marklein@riseup.net>
2019-07-27 17:09:30 +0200

    initial commit

---
+++ tmp/test2.js
@@ -0,0 +1,7 @@
+console.log('%c Hello', 'color: orange');
+
+const testArray = [{ test: 1 }, { test: 1 }, { test: 1 }];
+
+// console.table({ testArray });
+console.log({ testArray });
+console.log(testArray);
