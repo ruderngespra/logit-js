f2f515aceff089450509686119fb4c4c3ecbf009
janko <janko_marklein@riseup.net>
2019-08-09 14:23:29 +0200

    Minor test file changes

--- samples/testCode_2_before.js
+++ samples/testCode_2_before.js
@@ -8,3 +8,25 @@
     doubleX = timesTwo(x);
     var numbers;
 })();
+
+(function() {
+    var x;
+    var doubleX;
+    x = 23;
+    function timesTwo(num) {
+        return num * 2;
+    }
+    doubleX = timesTwo(x);
+    var numbers;
+})();
+
+(function() {
+    var x;
+    var doubleX;
+    x = 23;
+    function timesTwo(num) {
+        return num * 2;
+    }
+    doubleX = timesTwo(x);
+    var numbers;
+})();
