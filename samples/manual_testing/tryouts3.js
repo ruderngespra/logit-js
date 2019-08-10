15af8276fc93d543a68142cca03c856bbc232958
janko <janko_marklein@riseup.net>
2019-08-10 20:50:36 +0200

    Restructure lineNumber functionality

--- samples/manual_testing/tryouts3.js
+++ samples/manual_testing/tryouts3.js
@@ -1,5 +1,3 @@
 app.post('/delete/:id', function(req, res) {});
 
-function hello(sdf, sasf) {
-    1 + 1;
-}
+function hello(sdf, sasf) {}
--- samples/manual_testing/tryouts3.logit.js
+++ samples/manual_testing/tryouts3.logit.js
@@ -1,4 +1,9 @@
-hel => {
-    console.log('hel: ', hel);
-    return myModule;
-};
+app.post('/delete/:id', function (req, res) {
+  console.log("2:4", 'res: ', res);
+  console.log("3:4", 'req: ', req);
+});
+
+function hello(sdf, sasf) {
+  console.log("7:4", 'sasf: ', sasf);
+  console.log("8:4", 'sdf: ', sdf);
+}
