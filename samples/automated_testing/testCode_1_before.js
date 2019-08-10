b384145ce9c6d3c284423a8223f95a3fa990c96a
janko <janko_marklein@riseup.net>
2019-08-11 01:43:06 +0200

    remove unstable lineNumber feature

--- samples/automated_testing/testCode_1_before.js
+++ samples/automated_testing/testCode_1_before.js
@@ -1,9 +1,25 @@
+const { express } = require('hey');
+const { sdf } = require('hey');
+const { sdafsdfsad } = require('hey');
+
+const app = express();
+
+app.use(express.static('/projects'));
+
+app.listen(8080, () => {
+    console.log('Listening on 8080.');
+});
+
 function test() {
     var hello = 3;
 }
 
 function test2(hey, ho, he) {
+    console.log('he :', he);
+    console.log('ho :', ho);
+    console.log('hey :', hey);
     var hello = 3;
+    console.log('hello :', hello);
 }
 
 function test3() {
@@ -13,3 +29,5 @@ function test3() {
 function test4() {
     var hello = 3;
 }
+
+const gree = (heyse, eh, ehh) => heyse;
--- samples/automated_testing/testCode_1_before.logit.js
+++ samples/automated_testing/testCode_1_before.logit.js
@@ -2,18 +2,20 @@ function test() {
   var hello = 3;
 }
 
-function test2(hey) {
-  console.log("6:2 ", "hey: ", hey);
+function test2(hey, ho, he) {
+  console.log("6:2 ", "he: ", he);
+  console.log("7:2 ", "ho: ", ho);
+  console.log("8:2 ", "hey: ", hey);
   var hello = 3;
-  console.log("8:2 ", "hello: ", hello);
+  console.log("10:2 ", "hello: ", hello);
 }
 
 function test3() {
   var hello = 3;
-  console.log("13:2 ", "hello: ", hello);
+  console.log("hello: ", hello);
 }
 
 function test4() {
   var hello = 3;
-  console.log("18:2 ", "hello: ", hello);
+  console.log("hello: ", hello);
 }
